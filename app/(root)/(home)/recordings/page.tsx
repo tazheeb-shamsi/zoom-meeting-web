7ff160e00bdb8d5a27a159aa9cf0274d00259d63
tazheeb shamsi <thshamsi@hotmail.com>
2024-04-01 14:01:54 +0530

    FEAT: added sidebar & mobile navbar

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Recordings = () => {
+  return (
+    <section className="size-full flex flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Recordings</h1>
+    </section>
+  );
+};
+
+export default Recordings;
